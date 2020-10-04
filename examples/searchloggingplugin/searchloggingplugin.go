f311c639adc8fb0158c2ac646eaea91a60cf9472
Jonathan Cooper <cooper@cooperthecoder.com>
2020-10-04 14:25:54 -0500

    Added examples of RequestTransformer and ResponseTransformer to example plugin

--- examples/searchloggingplugin/searchloggingplugin.go
+++ examples/searchloggingplugin/searchloggingplugin.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	"github.com/joncooperworks/judas"
 )
@@ -23,4 +26,23 @@ func (p *searchLoggingPlugin) Listen(exchanges <-chan *judas.HTTPExchange) {
 // New returns a plugin that logs google searches.
 func New(logger *log.Logger) (judas.Listener, error) {
 	return &searchLoggingPlugin{logger: logger}, nil
+}
+
+// RequestTransformer replaces a victim's search query with something else if they search for the words "modify request".
+func RequestTransformer(request *http.Request) error {
+	if request.URL.Query().Get("q") == "modify request" {
+		query := request.URL.Query()
+		query.Set("q", "not what you searched for")
+		request.URL.RawQuery = query.Encode()
+	}
+	return nil
+}
+
+// ResponseTransformer replaces the page contents with our text when a user searches for the word "replace".
+func ResponseTransformer(response *http.Response) error {
+	if response.Request.URL.Query().Get("q") == "replace" {
+		payload := []byte("payload")
+		response.Body = ioutil.NopCloser(bytes.NewReader(payload))
+	}
+	return nil
 }
