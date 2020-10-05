39aff8c261df92eb3e69749d9d8fa99cd7e6dc52
Jonathan Cooper <cooper@cooperthecoder.com>
2020-10-05 02:12:16 -0500

    Removed references to google

--- examples/searchloggingplugin/searchloggingplugin.go
+++ examples/searchloggingplugin/searchloggingplugin.go
@@ -13,7 +13,7 @@ type searchLoggingPlugin struct {
 	logger *log.Logger
 }
 
-// Listen pulls google search queries out of HTTP exchanges
+// Listen pulls search queries out of HTTP exchanges
 func (p *searchLoggingPlugin) Listen(exchanges <-chan *judas.HTTPExchange) {
 	for exchange := range exchanges {
 		searchQuery := exchange.Request.URL.Query().Get("q")
@@ -23,7 +23,7 @@ func (p *searchLoggingPlugin) Listen(exchanges <-chan *judas.HTTPExchange) {
 	}
 }
 
-// New returns a plugin that logs google searches.
+// New returns a plugin that logs searches.
 func New(logger *log.Logger) (judas.Listener, error) {
 	return &searchLoggingPlugin{logger: logger}, nil
 }
